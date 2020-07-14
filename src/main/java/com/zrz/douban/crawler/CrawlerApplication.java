022751e8806a875bc404601f6bc88f0d8cc0e615
曾荣智 <zengrongzhi@ppmoney.com>
2020-07-14 16:32:08 +0800

    feat: 初始项目

---
+++ src/main/java/com/zrz/douban/crawler/CrawlerApplication.java
@@ -0,0 +1,212 @@
+package com.zrz.douban.crawler;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.commons.codec.Charsets;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+public class CrawlerApplication {
+
+    @Data
+    @NoArgsConstructor
+    @Builder
+    @AllArgsConstructor
+    static class Book {
+        private String title;
+        private float score;
+        private int people;
+        private String link;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @Builder
+    @AllArgsConstructor
+    static class SearchCriteria {
+        private String tagName;
+        private float score;
+        private int people;
+
+    }
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException, TimeoutException {
+        // 初始化线程池
+        Executor executor = new ThreadPoolExecutor(3, 3, 30, TimeUnit.SECONDS
+                , new LinkedBlockingQueue<>(1000)
+                , new ThreadFactory() {
+            private final AtomicInteger threadCount = new AtomicInteger(0);
+
+            @Override
+            public Thread newThread(Runnable r) {
+                return new Thread("crawler-async-executor-" + threadCount.incrementAndGet());
+            }
+        }
+                , new ThreadPoolExecutor.AbortPolicy()
+        );
+
+        long startTime = System.currentTimeMillis();
+        List<SearchCriteria> criteriaList = new ArrayList<>(Arrays.asList(
+                new SearchCriteria("生活", 8.5f, 2000),
+                new SearchCriteria("生活", 8.5f, 5000),
+                new SearchCriteria("生活", 9f, 2000),
+                new SearchCriteria("生活", 9f, 10000),
+                new SearchCriteria("科技", 8.5f, 2000),
+                new SearchCriteria("科技", 8.5f, 5000),
+                new SearchCriteria("科技", 9f, 2000),
+                new SearchCriteria("科技", 9f, 10000),
+                new SearchCriteria("文化", 8.5f, 2000),
+                new SearchCriteria("文化", 8.5f, 5000),
+                new SearchCriteria("文化", 9f, 2000),
+                new SearchCriteria("文化", 9f, 10000),
+                new SearchCriteria("经管", 8.5f, 2000),
+                new SearchCriteria("经管", 8.5f, 5000),
+                new SearchCriteria("经管", 9f, 2000),
+                new SearchCriteria("经管", 9f, 10000)
+        ));
+
+        criteriaList.forEach(criteria -> {
+            ConcurrentHashMap<String, Book> crawlResult = new ConcurrentHashMap<>(500);
+            List<String> linkList = getTagLinks(criteria.tagName);
+
+            CompletableFuture<Void> future = null;
+            for (String url : linkList) {
+                future = CompletableFuture.runAsync(() -> {
+                    parseBookDesc(url, criteria, crawlResult);
+                });
+            }
+
+            CompletableFuture.allOf(future).join();
+            String fileName = String.format("%s-%s-%s.csv", criteria.getTagName(), criteria.getScore(), criteria.getPeople());
+            System.out.println(String.format("----------------开始保存数据:%s\n", fileName));
+            StringBuilder stringBuilder = new StringBuilder("标题,评分,人数,链接\n");
+            for (Map.Entry<String, Book> entry : crawlResult.entrySet()) {
+                stringBuilder.append(String.format("%s,%s,%s,%s\n", entry.getKey(), entry.getValue().getScore(), entry.getValue().getPeople(), entry.getValue().getLink()));
+            }
+
+            try (FileOutputStream outputStream = new FileOutputStream(fileName)) {
+                outputStream.write(stringBuilder.toString().getBytes(Charsets.UTF_8));
+                System.out.println(String.format("----------------数据保存成功:%s\n", fileName));
+            } catch (IOException e) {
+                System.out.println(String.format("----------------数据保存失败！！！:%s\n", fileName));
+            }
+
+        });
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("程序运行时间：" + (endTime - startTime) + "ms");
+    }
+
+    private static List<String> getTagLinks(String tagName) {
+        String baseURL = "https://book.douban.com";
+        String startURL = baseURL + "/tag/?view=type&icn=index-sorttags-all";
+        List<String> linkList = new ArrayList<>();
+        Document doc = getDoc(startURL);
+        doc.select("#content > div > div.article > div:nth-child(2) a[name=" + tagName + "]").next().select("a").forEach(x -> {
+            linkList.add(baseURL + x.attr("href"));
+        });
+
+        return linkList;
+    }
+
+    /**
+     * 通过url获取符合指定筛选条件的书籍信息
+     *
+     * @param url            请求地址
+     * @param searchCriteria 筛选条件
+     * @param crawlerResult  抓取结果
+     */
+    private static void parseBookDesc(String url, SearchCriteria searchCriteria, ConcurrentHashMap<String, Book> crawlerResult) {
+        int pageCount = 0;
+        Document doc = getDoc(url);
+
+        try {
+            pageCount = Integer.parseInt(doc.select("#subject_list > div.paginator >a").last().text());
+        } catch (Exception ex) {
+            System.out.println("url:" + url + ",页码转换错误:" + ex.getMessage());
+            pageCount = 1;
+        }
+
+        CompletableFuture<Void> completableFuture = null;
+        for (int i = 0; i < pageCount; i++) {
+            final int finalI = i;
+            completableFuture = CompletableFuture.runAsync(() -> {
+                String pagedListUrl = String.format("%s?start%s&type=T", url, finalI * 20);
+                Document pagedListDoc = getDoc(pagedListUrl);
+                pagedListDoc.select("#subject_list > ul > li").forEach(ele -> {
+                    String title = ele.select(".info>h2>a").text()
+                            .replace("\n", "")
+                            .replace(" ", "");
+                    String link = ele.select(".info>h2>a").attr("href");
+                    float score = 0f;
+                    try {
+                        score = Float.parseFloat(ele.select(".rating_nums").text());
+                    } catch (Exception ignored) {
+                    }
+
+                    int peopleNum = 0;
+                    try {
+                        String peopleNumStr = ele.select(".pl").text();
+                        peopleNumStr = peopleNumStr.replace(" ", "")
+                                .replace("(", "")
+                                .replace("\n", "")
+                                .replace("人评价)", "");
+                        peopleNum = Integer.parseInt(peopleNumStr);
+
+                    } catch (Exception ignored) {
+                    }
+
+                    if (score >= searchCriteria.getScore() && peopleNum >= searchCriteria.getPeople()) {
+                        Book book = new Book(title, score, peopleNum, link);
+                        if (crawlerResult.putIfAbsent(book.title, book) == null) {
+                            System.out.println(String.format("%s,%s,%s", book.getTitle(), book.getScore(), book.getPeople()));
+                        }
+                    }
+                });
+            });
+
+        }
+
+
+        CompletableFuture.allOf(completableFuture).join();
+
+    }
+
+    /**
+     * 通过url获取Document对象
+     *
+     * @param url 请求地址
+     * @return Document对象
+     */
+    private static Document getDoc(String url) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("user-agent", "Chrome/83.0");
+//        headers.put("Cookie", "__yadk_uid=");
+        String html = HttpClientUtil.getInstance().get(url, headers);
+        return Jsoup.parse(html);
+    }
+}
---
+++ src/main/java/com/zrz/douban/crawler/HttpClientUtil.java
@@ -0,0 +1,287 @@
+package com.zrz.douban.crawler;
+
+import org.apache.commons.codec.Charsets;
+import org.apache.http.HeaderElement;
+import org.apache.http.HeaderElementIterator;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.conn.ConnectionKeepAliveStrategy;
+import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicHeaderElementIterator;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class HttpClientUtil implements AutoCloseable {
+
+    private volatile static HttpClientUtil httpClientUtil;
+    private final CloseableHttpClient httpClient;
+    private final PoolingHttpClientConnectionManager connMgr;
+    private final ThreadLocal<HttpClientContext> httpContext = ThreadLocal.withInitial(HttpClientContext::create);
+    private final IdleConnectionEvictThread evictThread;
+
+    public HttpClientUtil() {
+
+        // 自定义keep-alive策略，keep-alive使得tcp连接可以被复用。
+        // 但默认的keep-alive时长为无穷大，不符合现实，所以需要改写，定义一个更短的时间
+        // 如果服务器http响应头包含 "Keep-Alive:timeout=" ，则使用timeout=后面指定的值作为keep-alive的时长，否则默认60秒
+        ConnectionKeepAliveStrategy strategy = (httpResponse, httpContext) -> {
+            HeaderElementIterator it = new BasicHeaderElementIterator(httpResponse.headerIterator(HTTP.CONN_KEEP_ALIVE));
+            while (it.hasNext()) {
+                HeaderElement he = it.nextElement();
+                String param = he.getName();
+                String value = he.getValue();
+                if (value != null && param.equalsIgnoreCase("timeout")) {
+                    return Long.parseLong(value) * 1000;
+                }
+            }
+
+            return 60 * 1000;
+
+        };
+
+        // 自定义连接池，连接池可以连接可以使连接可以被复用。
+        // 连接的复用需要满足：Keep-alive + 连接池。keep-alive使得连接能够保持存活，不被系统销毁；连接池使得连接可以被程序重复引用
+        // 默认的连接池，DefaultMaxPerRoute只有5个，MaxTotal只有10个，不满足实际的生产需求
+        connMgr = new PoolingHttpClientConnectionManager();
+
+        // 最大连接数500
+        connMgr.setMaxTotal(500);
+        // 同一个ip:port的请求，最大连接数200
+        connMgr.setDefaultMaxPerRoute(200);
+
+        // 启动线程池空闲连接、超时连接监控线程
+        evictThread = new IdleConnectionEvictThread(connMgr);
+        evictThread.start();
+
+        // 定义请求超时配置
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(5000)  // 从连接池里获取连接的超时时间
+                .setConnectTimeout(2000)            // 建立TCP的超时时间
+                .setSocketTimeout(10000)             // 读取数据包的超时时间
+                .build();
+
+
+        httpClient = HttpClients.custom()
+                .setConnectionManager(connMgr)
+                .setKeepAliveStrategy(strategy)
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
+    }
+
+    /**
+     * 因为HttpClient是线程安全的，可以提供给多个线程复用，同时连接池的存证的目的就是为了可以复用连接，所以提供单例模式
+     */
+    private static class HttpClientUtilHolder {
+        private static final HttpClientUtil INSTANCE = new HttpClientUtil();
+    }
+
+    public static HttpClientUtil getInstance() {
+        return HttpClientUtilHolder.INSTANCE;
+    }
+
+    public PoolingHttpClientConnectionManager getConnMgr() {
+        return connMgr;
+    }
+
+    public HttpContext getHttpContext() {
+        return httpContext.get();
+    }
+
+    public CloseableHttpClient getHttpClient() {
+        return httpClient;
+    }
+
+    /**
+     * http get操作
+     * @param url       请求地址
+     * @param headers   请求头
+     * @return          返回响应内容
+     */
+    public String get(String url, Map<String, String> headers) {
+        HttpGet httpGet = new HttpGet(url);
+        if (headers != null) {
+            for (Map.Entry<String, String> header : headers.entrySet()) {
+                httpGet.setHeader(header.getKey(), header.getValue());
+            }
+        }
+
+        CloseableHttpResponse response = null;
+
+        try {
+            response = httpClient.execute(httpGet, httpContext.get());
+            HttpEntity entity = response.getEntity();
+            return EntityUtils.toString(entity, Charsets.UTF_8);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (response != null) {
+                try {
+                    //上面的EntityUtils.toString会调用inputStream.close(),进而也会触发连接释放回连接池，但因为httpClient.execute可能抛异常，所以得在finally显示调一次，确保连接一定被释放
+                    response.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * form表单post
+     *
+     * @param url     请求地址
+     * @param params  参数内容
+     * @param headers http头信息
+     * @return http文本格式响应内容
+     */
+    public String postWithForm(String url, Map<String, String> params, Map<String, String> headers) {
+        HttpPost httpPost = new HttpPost(url);
+        if (headers != null) {
+            for (Map.Entry<String, String> header : headers.entrySet()) {
+                httpPost.setHeader(header.getKey(), header.getValue());
+            }
+        }
+
+        List<NameValuePair> formParams = new ArrayList<>();
+        if (params != null) {
+            for (Map.Entry<String, String> param : params.entrySet()) {
+                formParams.add(new BasicNameValuePair(param.getKey(), param.getValue()));
+            }
+        }
+
+        UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(formParams, Charsets.UTF_8);
+        httpPost.setEntity(formEntity);
+        CloseableHttpResponse response = null;
+
+        try {
+            response = httpClient.execute(httpPost, httpContext.get());
+            HttpEntity entity = response.getEntity();
+            return EntityUtils.toString(entity, Charsets.UTF_8);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (response != null) {
+                try {
+                    response.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * json内容post
+     *
+     * @param url     请求地址
+     * @param data    json报文
+     * @param headers http头
+     * @return http文本格式响应内容
+     */
+    public String postWithBody(String url, String data, Map<String, String> headers) {
+        HttpPost httpPost = new HttpPost(url);
+        if (headers != null) {
+            for (Map.Entry<String, String> header : headers.entrySet()) {
+                httpPost.setHeader(header.getKey(), header.getValue());
+            }
+        }
+
+        StringEntity stringEntity = new StringEntity(data, ContentType.create("plain/text", Charsets.UTF_8));
+        httpPost.setEntity(stringEntity);
+        httpPost.setHeader("Content-type", "application/json");
+        CloseableHttpResponse response = null;
+
+        try {
+            response = httpClient.execute(httpPost, httpContext.get());
+            HttpEntity entity = response.getEntity();
+            return EntityUtils.toString(entity, Charsets.UTF_8);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (response != null) {
+                try {
+                    response.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * HttpClientUtil类回收时，通过httpClient.close()，可以间接的关闭连接池，从而关闭连接池持有的所有tcp连接
+     * 与response.close()的区别：response.close()只是把某个请求持有的tcp连接放回连接池，而httpClient().close()是销毁整个连接池
+     *
+     * @throws IOException
+     */
+    @Override
+    public void close() throws IOException {
+        httpClient.close();
+        evictThread.shutdown();
+    }
+
+    /**
+     * 定义一个连接监控线程类，用以从连接池里关闭过期的连接（即服务器已经关闭的连接），以及在30秒内处于空闲的连接；每5秒钟处理一次
+     */
+    static class IdleConnectionEvictThread extends Thread {
+        private final HttpClientConnectionManager connMgr;
+        private volatile boolean shutdown;
+
+        public IdleConnectionEvictThread(HttpClientConnectionManager connMgr) {
+            super();
+            this.connMgr = connMgr;
+            setDaemon(true);
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (!shutdown) {
+                    synchronized (this) {
+                        wait(5000);
+                        connMgr.closeExpiredConnections();
+                        connMgr.closeIdleConnections(30, TimeUnit.SECONDS);
+                    }
+                }
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        /**
+         * 关闭监控线程
+         */
+        public void shutdown() {
+            shutdown = true;
+            // 监控线程可能还处于wait()状态，通过notifyAll()唤醒，及时退出while循环
+            synchronized (this) {
+                notifyAll();
+            }
+        }
+    }
+}
